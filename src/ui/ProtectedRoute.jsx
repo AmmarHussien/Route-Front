b3f438243173d04884dec7ecea3d6dfd6ac07a7d
Ammar Hussien <93228555+AmmarHussien@users.noreply.github.com>
2024-11-08 18:07:21 +0200

    enhance

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -27,7 +27,7 @@ function ProtectedRoute({ children }) {
   // 2. if there is no authenticated user , redirect to the login page
 
   useEffect(() => {
-    if (!isAuthenticated) navigate("/adminPanel/login");
+    if (!isAuthenticated) navigate("/login");
   }, [isAuthenticated, navigate]);
 
   // 3. while loading, show spinner
